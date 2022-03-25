63b5a1f6ff186ae08e1bb87b5600af08bad0c2ba
Felipe Mascarenhas <felipemk10@gmail.com>
2022-03-25 07:30:32 -0300

    feat: Adicionado os itens do carrinho

--- pizza_base/script.js
+++ pizza_base/script.js
@@ -192,8 +192,31 @@ function updateCart(){
 
       let cartItem = c('.models .cart--item').cloneNode(true);
 
+
+      let pizzaSizeName;
+
+      switch(cart[i].size){
+
+        case 0:
+          pizzaSizeName = 'P';
+          break;
+        case 1:
+          pizzaSizeName = 'M';
+          break;
+        case 2: 
+          pizzaSizeName = 'G';
+          break;
+
+
+
+      }
+
+
+      let pizzaName = `${pizzaItem.name} (${pizzaSizeName})`;
+
       cartItem.querySelector('img').src = pizzaItem.img;
-      cartItem.querySelector('.cart--item-nome').innerHTML = pizzaItem.name;
+      cartItem.querySelector('.cart--item-nome').innerHTML = pizzaName;
+      cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qt;
 
       c('.cart').append(cartItem);
 
